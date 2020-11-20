1586dc7c71782a8c6d683782b957d350c6f52d91
Brayan Rivas <bragiova.rivas@gmail.com>
2020-11-19 18:30:03 -0600

    Fin seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,17 +8,27 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  equipo: any[] = [];
   cargada = false;
 
   constructor( private http: HttpClient) {
-    console.log('Servicio infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     // Leer archivo JSON
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
     });
+  }
 
+  private cargarEquipo() {
+    this.http.get('https://angular-html-21af3.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+      this.equipo = resp;
+    });
   }
 }
