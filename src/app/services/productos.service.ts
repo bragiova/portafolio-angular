7c5a14b3554a23e7acb987461d778fc04b4106b5
Brayan Rivas <bragiova.rivas@gmail.com>
2020-11-19 20:17:27 -0600

    Fin seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,17 +9,53 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-21af3.firebaseio.com/productos_idx.json')
+    return new Promise((resolve, reject) => {
+      this.http.get('https://angular-html-21af3.firebaseio.com/productos_idx.json')
       .subscribe( (resp: Producto[]) => {
-        console.log(resp);
         this.cargando = false;
         this.productos = resp;
+        resolve();
       });
+    });
+  }
+
+  getProducto(id: string) {
+    return this.http.get(`https://angular-html-21af3.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarProducto(termino: string) {
+
+    if (this.productos.length === 0) {
+      // cargar productos
+      this.cargarProductos().then(() => {
+        // ejecutar después de tener los productos
+        // aplicar filtro
+        this.filtrarProductos(termino);
+      });
+    }
+    else {
+      // aplicar el filtro
+      this.filtrarProductos(termino);
+    }
+  }
+
+  private filtrarProductos(termino: string) {
+    this.productosFiltrado = [];
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLowerCase();
+      // si contiene algo el texto de la búsqueda que coincida con la categoría del producto
+      if (prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0) {
+        this.productosFiltrado.push(prod);
+      }
+    });
   }
 }
